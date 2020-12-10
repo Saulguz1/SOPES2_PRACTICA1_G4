454cfe6693e6415fe4366b46b8f3a153d33eab04
Oscar Corleto <oscarcorleto000@gmail.com>
2020-12-10 00:06:24 -0600

    index.js y GET memoria RAM

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,9 +1,113 @@
-var http = require('http');
+const ip = 'http://34.69.2.19/'
+let ejex = ""
+let usedram = ""
+let totalram = ""
+let freeram = ""
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  
-  res.end('Hello World!');
-}).listen(8080);
+function cargarram(){
+  ejex = sessionStorage.getItem("ejex");
+  usedram = sessionStorage.getItem("usedram");
+  totalram = sessionStorage.getItem("totalram");
+  freeram = sessionStorage.getItem("freeram");
 
-console.log("corriendo en puerto 8080");
+  $.ajax({
+    type:"GET",
+    url:ip+"ram",
+    crossDomain:true,
+    success : function(data,status,err){
+      if(status == 'success'){
+        var json = JSON.parse(JSON.stringify(data).replace("RowDataPacket ",""));
+        if (ejex == null) ejex=""
+        if (usedram == null) usedram=""
+        if (totalram == null) totalram=""
+        if (freeram == null) freeram=""
+
+        ejex += ""+","
+        usedram += json.used +","
+        totalram += json.total+","
+        freeram += json.free+","
+        
+        pintargraph(ejex.split(","),usedram.split(","),totalram.split(","),freeram.split(","))
+
+        sessionStorage.setItem("ejex", ejex);
+        sessionStorage.setItem("usedram", usedram);
+        sessionStorage.setItem("totalram", totalram);
+        sessionStorage.setItem("freeram", freeram);
+      }
+      else console.log(err)
+    }
+  });
+}
+
+
+
+function cargarcpu(){
+  sessionStorage.clear("ejex");
+  sessionStorage.clear("usedram");
+  sessionStorage.clear("totalram");
+  sessionStorage.clear("freeram");
+  alert("variables limpiecitas")
+}
+
+function pintargraph(varejex,varused,vartotal,varfree){
+  varejex.pop()
+  varused.pop()
+  vartotal.pop()
+  varfree.pop()
+  var config = {
+    type: 'line',
+    data: {
+        labels: varejex,
+        datasets: [{
+            label: 'Usada',
+            backgroundColor: '#FFFFFF',
+            borderColor: 'green',
+            data: varused,
+            fill: false,
+        },{
+            label: 'Libre',
+            backgroundColor: '#FFFFFF',
+            borderColor: 'red',
+            data: varfree,
+            fill: false,
+        },{
+            label: 'Total',
+            backgroundColor: '#FFFFFF',
+            borderColor: 'blue',
+            data: vartotal,
+            fill: false,
+        }]
+    },
+    options: {
+        responsive: true,
+
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString:'Ram'
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Porcentage'
+                }
+            }]
+        }
+    }
+  };
+
+  var ctx = document.getElementById('canvas').getContext('2d');
+  window.myLine = new Chart(ctx, config);
+}
